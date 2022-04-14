953364b4fc9a157a94e3a8c9d5ceea65b6a10080
Texmo100 <isaac.ir@hotmail.com>
2022-04-13 17:13:32 -0700

    v-if and class binding added to the counter

--- src/components/Counter/Counter.vue
+++ src/components/Counter/Counter.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="counter">
-    {{ count }}
+    <p :class=[indicatorClass]>{{ count }}</p>
+    <span v-if="limitReachedDetector" class="limit-reached">Limit reached !</span>
   </div>
 </template>
 
@@ -9,6 +10,14 @@ export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Counter",
   props: ['count'],
+  computed: {
+    indicatorClass() {
+      return this.count >= 10 ? 'indicator alert' : 'indicator'
+    },
+    limitReachedDetector() {
+      return this.count >= 10 ? true : false;
+    },
+  },
 };
 </script>
 
@@ -21,8 +30,9 @@ export default {
 .counter {
   justify-content: center;
   align-items: center;
-  font-size: 5rem;
   border-right: 1px solid #e8e8e8;
+  display: flex;
+  flex-direction: column;
 }
 
 @media screen and (max-width: 768px) {
@@ -30,5 +40,17 @@ export default {
     border: none;
     border-bottom: 1px solid #e8e8e8;
   }
+}
+
+.indicator {
+  font-size: 5rem;
+}
+
+.alert {
+  color: #E30B5C;
+}
+
+.limit-reached {
+  color: #E30B5C;
 }
 </style>
